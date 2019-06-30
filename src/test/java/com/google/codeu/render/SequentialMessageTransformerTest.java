17c6435b9f4f983194b79fa0b15c32df8d9dd611
Adel Wu <adelwu@seas.upenn.edu>
2019-06-30 16:42:17 -0500

    change message to review in test folder

--- src/test/java/com/google/codeu/render/SequentialMessageTransformerTest.java
+++ src/test/java/com/google/codeu/render/SequentialMessageTransformerTest.java
@@ -2,62 +2,62 @@ package com.google.codeu.render;
 
 import static org.junit.Assert.assertEquals;
 
-import com.google.codeu.data.Message;
+import com.google.codeu.data.Review;
 import java.util.Arrays;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class SequentialMessageTransformerTest {
+public class SequentialReviewTransformerTest {
 
   @Test
   /** Tests that when there are no delegates, the input is return untransformed. */
   public void testTransformTextNoDelegates() {
     // Constructs the class under test.
-    MessageTransformer messageTransformer = new SequentialMessageTransformer(Arrays.asList());
+    ReviewTransformer reviewTransformer = new SequentialReviewTransformer(Arrays.asList());
 
-    Message message0 = new Message("user0", "an input text");
+    Review review0 = new Review("user0", "an input text");
 
-    assertEquals(message0, messageTransformer.transform(message0));
+    assertEquals(review0, reviewTransformer.transform(review0));
   }
 
   @Test
   /** Tests when there is exactly one delegate, its output is returned. */
   public void testTransformTextOneDelegate() {
-    MessageTransformer mockDelegate = Mockito.mock(MessageTransformer.class);
+    ReviewTransformer mockDelegate = Mockito.mock(ReviewTransformer.class);
 
-    Message message0 = new Message("user0", "an input text");
-    Message message1 = new Message("user1", "mock output 1");
+    Review review0 = new Review("user0", "an input text");
+    Review review1 = new Review("user1", "mock output 1");
 
-    Mockito.when(mockDelegate.transform(message0)).thenReturn(message1);
+    Mockito.when(mockDelegate.transform(review0)).thenReturn(review1);
 
     // Constructs the class under test.
-    MessageTransformer messageTransformer =
-        new SequentialMessageTransformer(Arrays.asList(mockDelegate));
+    ReviewTransformer reviewTransformer =
+        new SequentialReviewTransformer(Arrays.asList(mockDelegate));
 
-    assertEquals(message1, messageTransformer.transform(message0));
+    assertEquals(review1, reviewTransformer.transform(review0));
   }
 
   @Test
   /** Tests a sequence of three delegates are run correctly. */
   public void testTransformTextSequenceOfThree() {
-    MessageTransformer mockDelegate1 = Mockito.mock(MessageTransformer.class);
-    MessageTransformer mockDelegate2 = Mockito.mock(MessageTransformer.class);
-    MessageTransformer mockDelegate3 = Mockito.mock(MessageTransformer.class);
+    ReviewTransformer mockDelegate1 = Mockito.mock(ReviewTransformer.class);
+    ReviewTransformer mockDelegate2 = Mockito.mock(ReviewTransformer.class);
+    ReviewTransformer mockDelegate3 = Mockito.mock(ReviewTransformer.class);
 
-    Message message0 = new Message("user0", "an input text");
-    Message message1 = new Message("user1", "mock output 1");
-    Message message2 = new Message("user2", "mock output 2");
-    Message message3 = new Message("user3", "mock output 3");
+    Review review0 = new Review("user0", "an input text");
+    Review review1 = new Review("user1", "mock output 1");
+    Review review2 = new Review("user2", "mock output 2");
+    Review review3 = new Review("user3", "mock output 3");
 
-    Mockito.when(mockDelegate1.transform(message0)).thenReturn(message1);
-    Mockito.when(mockDelegate2.transform(message1)).thenReturn(message2);
-    Mockito.when(mockDelegate3.transform(message2)).thenReturn(message3);
+    Mockito.when(mockDelegate1.transform(review0)).thenReturn(review1);
+    Mockito.when(mockDelegate2.transform(review1)).thenReturn(review2);
+    Mockito.when(mockDelegate3.transform(review2)).thenReturn(review3);
 
     // Constructs the class under test.
-    MessageTransformer messageTransformer =
-        new SequentialMessageTransformer(
+    ReviewTransformer reviewTransformer =
+        new SequentialReviewTransformer(
             Arrays.asList(mockDelegate1, mockDelegate2, mockDelegate3));
 
-    assertEquals(message3, messageTransformer.transform(message0));
+    assertEquals(review3, reviewTransformer.transform(review0));
   }
 }
