ef630e315daf71ef02440c3c4ffd6a9e0ba4e5eb
Dan Garrette <dhgarrette@gmail.com>
2019-07-16 01:13:42 -0400

    Add sentiment analysis to reviews (adapted from Kingsley's PR) (#33)

---
+++ src/main/java/com/google/codeu/render/SentimentReviewTransformer.java
@@ -0,0 +1,32 @@
+package com.google.codeu.render;
+
+import com.google.codeu.analysis.SentimentAnalyzer;
+import com.google.codeu.data.Review;
+
+/** Replaces image Urls with HTML image elements. */
+public class SentimentReviewTransformer implements ReviewTransformer {
+
+  private SentimentAnalyzer sentimentAnalyzer;
+
+  public SentimentReviewTransformer() {
+    sentimentAnalyzer = SentimentAnalyzer.getInstance();
+  }
+
+  @Override
+  public Review transform(Review review) {
+    String text = review.getText();
+    text +=
+        "\n\n<p style=\"color: grey\">"
+            + "Sentiment score: "
+            + sentimentAnalyzer.score(text)
+            + "</p>";
+
+    return new Review(
+        review.getId(),
+        review.getUser(),
+        text,
+        review.getTimestamp(),
+        review.getHub(),
+        review.getRating());
+  }
+}
